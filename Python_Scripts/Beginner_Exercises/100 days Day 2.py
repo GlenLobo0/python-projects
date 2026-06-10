f56eb119f823ecd6c228812ac309ed75ad1232d6
Glen Lobo0 <Glenlobo2007@gmail.com>
2026-06-10 21:34:52 +0530

    Remove unused folders

--- Python_Scripts/Beginner_Exercises/100 days Day 2.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the Tip calculator")
-bill =float(input("How much was the bill:$"))
-tip =int(input("What amount of percentaage would you like to give for ur food(10,20,30):"))
-people=int(input("how many people are there to split up with: "))
-bill_withtip = bill*(tip/100) + bill
-divide_withpeople =bill_withtip/people
-roundup=round(divide_withpeople, 2)
-print(f"Each of yáll(or your lonely ass) has/have to pay ${roundup}")
--- Python_Scripts/Beginner_Exercises/100 days of python Day 1.py
+++
@@ -1,6 +0,0 @@
-
-print("Welcome to the band name gen")
-city = input("Which city lil bro:\n")
-print(city)
-pet = input("What is ur pets name:\n")
-print("Your band name could be"+ " "+ city + " " + pet)
