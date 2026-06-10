89ff9be04710d10aa7fc91bb463d7aef41b46908
Glen Lobo0 <Glenlobo2007@gmail.com>
2026-06-10 21:32:03 +0530

    Remove root files and organize scripts

--- firstprogram.py
+++
@@ -1,5 +0,0 @@
-first = int(input("enter first:"))
-
-second = int(input("enter second:"))
-jls_extract_var = "sum ="
-print(jls_extract_var, first + second)
--- lecture2.py
+++
@@ -1,9 +0,0 @@
-str = int(input("Enter your marks:"))
-if str >= 90:
-    print("grade = A")
-elif str >= 80:
-    print("grade = B")
-elif str >= 70:
-    print("grade = C")
-else:
-    print("grade = D")
